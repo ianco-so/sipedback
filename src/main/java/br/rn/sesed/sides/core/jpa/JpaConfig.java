0b73a24fe5fb1c50892bd4c6d8bb54125deed65c
Emiliano Loiola <emiliano.grn@gmail.com>
2022-12-13 21:00:06 -0300

    Inicio do Projeto - Sides
    Version: 1.0.0
    Date: 2022-12-10

---
+++ src/main/java/br/rn/sesed/sides/core/jpa/JpaConfig.java
@@ -0,0 +1,22 @@
+package br.rn.sesed.sides.core.jpa;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+
+@Configuration
+public class JpaConfig {
+
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		DataSource dataSource = dataSourceLookup.getDataSource("java:/rotafxDS");
+		return dataSource;
+	}
+	
+	
+}
