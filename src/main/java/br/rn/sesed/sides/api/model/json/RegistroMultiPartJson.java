ed09cceae08f9c8e834c5c08dd533ffe6fd62720
adriel <12868863418@ntxvmd01081.sesed.interno>
2023-04-25 10:30:56 -0300

    Modelo MultiPartForm recebido pela rota

---
+++ src/main/java/br/rn/sesed/sides/api/model/json/RegistroMultiPartJson.java
@@ -0,0 +1,14 @@
+package br.rn.sesed.sides.api.model.json;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class RegistroMultiPartJson {
+	
+	private RegistroJson registro;
+	private MultipartFile fotoPrincipal;
+	private MultipartFile segundaFoto;
+	private MultipartFile terceiraFoto;
+}
